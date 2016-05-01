898de1bcd3129a96a08172f38385cb25938acdea
pptk <pengxionghuidemacpro@pengxionghuideMacdeMacBook-Pro.local>
2016-05-01 13:40:25 +0800

    修改
    使用CAShaper，消除时针旋转时的锯齿。使用CAReplicatorLayer。

--- CenterAnimationDemo/CenterAnimation/ViewController.m
+++ CenterAnimationDemo/CenterAnimation/ViewController.m
@@ -15,112 +15,140 @@
 
 @interface ViewController ()
 
-@property(nonatomic)UIImageView *minuteHand;
-@property(nonatomic)UIImageView *hourHand;
-@property(nonatomic)UIImageView *secondHand;
-
-
-@property (nonatomic,weak) NSTimer *timer;
-
 @end
 
 @implementation ViewController
-
+{
+    CAShapeLayer *hourShapeLayer;
+    CAShapeLayer *minuShapeLayer;
+    CAShapeLayer *secondShapeLayer;
+    NSTimer *timer;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self initUI];
+    // Do any additional setup after loading the view.
+    
+    
+    [self initView];//初始化
+    
+    timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(ticker) userInfo:nil repeats:YES];
+    [self ticker];
 }
-#pragma mark 时钟
--(void)initUI{
-    //背景
-    CGFloat cornerWidth = 5;
-    CGFloat backView_wh = 200;
-    UIView *backView = [[UIView alloc]initWithFrame:CGRectMake((UISCREEN_WIDTH-200)/2, (UISCREEN_HEIGHT-200)/2, backView_wh, backView_wh)];
-    backView.backgroundColor = [UIColor clearColor];
-    backView.layer.borderColor = RandCOLOR.CGColor;
-    backView.layer.borderWidth = cornerWidth;
-    backView.layer.cornerRadius = backView_wh/2;
-    backView.layer.masksToBounds = YES;
+-(void)initView{
+    //背景圆圈
+    CALayer *backgroundLayer = [CALayer layer];
+    backgroundLayer.frame = CGRectMake(100, 100, 200, 200);
+    backgroundLayer.backgroundColor = [UIColor whiteColor].CGColor;
+    UIBezierPath *pathshaperLayer = [[UIBezierPath alloc]init];
+    [pathshaperLayer addArcWithCenter:CGPointMake(100, 100) radius:100 startAngle:0 endAngle:2*M_PI clockwise:YES];
+    CAShapeLayer *shaperLayer = [CAShapeLayer layer];//矢量
+    shaperLayer.strokeColor = [UIColor redColor].CGColor;
+    shaperLayer.fillColor = [UIColor clearColor].CGColor;
+    shaperLayer.lineWidth = 5;
+    shaperLayer.path = pathshaperLayer.CGPath;
+    [backgroundLayer addSublayer:shaperLayer];
     
-    //刻度
-    for(int i = 0;i<12;i++){
-        UIView *scaleMark = [[UIView alloc]init];
-        scaleMark.backgroundColor = RandCOLOR;
-        scaleMark.layer.anchorPoint = CGPointMake(0.5,(backView_wh/2-cornerWidth*2)/cornerWidth);
-        scaleMark.frame = CGRectMake((backView_wh-cornerWidth)/2, cornerWidth*2, cornerWidth, cornerWidth);
-        CGFloat transAngle = M_PI * 2/12 * i;
-        scaleMark.transform = CGAffineTransformMakeRotation(transAngle);
-        [backView addSubview:scaleMark];
-    }
+    //在该Layer上的所有控件会自动重复12个（节省内存，方便处理倒影等）。
+    CAReplicatorLayer *tickLayer = [CAReplicatorLayer layer];//表的刻度，在tickLayer内的每一个控件都会每次旋转30度，然后重复12个对象。
+    tickLayer.frame = backgroundLayer.bounds;
+    tickLayer.instanceCount = 12;
+    CATransform3D transform = CATransform3DIdentity;
+    transform = CATransform3DTranslate(transform, 0, 0, 0);
+    transform = CATransform3DRotate(transform, M_PI/6, 0, 0, 1);//z轴旋转30度。
+    transform = CATransform3DTranslate(transform, 0, 0, 0);
+    tickLayer.instanceTransform = transform;
+    CALayer *aTickLayer = [CALayer layer];
+    aTickLayer.frame = CGRectMake(98, 0, 4, 6);
+    aTickLayer.backgroundColor = [UIColor redColor].CGColor;
+    [tickLayer addSublayer:aTickLayer];
+    [backgroundLayer addSublayer:tickLayer];
     
-    [self.view addSubview:backView];
+    //指针
+    //时针
+    UIBezierPath *hourPath = [[UIBezierPath alloc]init];
+    [hourPath moveToPoint:CGPointMake(8, 50)];
+    [hourPath addLineToPoint:CGPointMake(0, 100)];
+    [hourPath addLineToPoint:CGPointMake(16, 100)];
+    [hourPath addLineToPoint:CGPointMake(8, 50)];
+    hourShapeLayer = [CAShapeLayer layer];
+    hourShapeLayer.anchorPoint = CGPointMake(0.5f, 1.0f);
+    hourShapeLayer.frame = CGRectMake(92, 0, 16, 100);
+    hourShapeLayer.strokeColor = [UIColor redColor].CGColor;
+    hourShapeLayer.fillColor = [UIColor redColor].CGColor;
+    hourShapeLayer.lineWidth = 1;
+    hourShapeLayer.path = hourPath.CGPath;
+    [backgroundLayer addSublayer:hourShapeLayer];
     
-    //三根针
-    self.secondHand = [[UIImageView alloc]init];
-    self.minuteHand = [[UIImageView alloc]init];
-    self.hourHand = [[UIImageView alloc]init];
-    [self.secondHand setBackgroundColor:RandCOLOR];
-    [self.minuteHand setBackgroundColor:RandCOLOR];
-    [self.hourHand setBackgroundColor:RandCOLOR];
-    //调整anchorpoint,调整完之后再设置Frame所以不会变形。
-    self.secondHand.layer.anchorPoint = CGPointMake(0.5f, 1);
-    self.minuteHand.layer.anchorPoint = CGPointMake(0.5f, 1);
-    self.hourHand.layer.anchorPoint = CGPointMake(0.5f, 1);
-    CGFloat secondHand_h = backView_wh/2 - 20;
-    CGFloat minuteHand_h = backView_wh/2 - 35;
-    CGFloat hourHand_h = backView_wh/2 - 50;
-    self.secondHand.frame = CGRectMake((backView_wh-3)/2, 20, 3, secondHand_h);//秒针
-    self.minuteHand.frame = CGRectMake((backView_wh-6)/2, 35, 6, minuteHand_h);//分针
-    self.hourHand.frame = CGRectMake((backView_wh-9)/2, 50, 9, hourHand_h);//时针
-    [backView addSubview:self.secondHand];
-    [backView addSubview:self.minuteHand];
-    [backView addSubview:self.hourHand];
+    //分针
+    UIBezierPath *minuPath = [[UIBezierPath alloc]init];
+    [minuPath moveToPoint:CGPointMake(5, 0)];
+    [minuPath addLineToPoint:CGPointMake(0, 60)];
+    [minuPath addLineToPoint:CGPointMake(10, 60)];
+    [minuPath addLineToPoint:CGPointMake(5, 0)];
+    minuShapeLayer = [CAShapeLayer layer];
+    minuShapeLayer.anchorPoint = CGPointMake(0.5f, 1.0f);
+    minuShapeLayer.frame = CGRectMake(95, 40, 10, 60);
+    minuShapeLayer.strokeColor = [UIColor greenColor].CGColor;
+    minuShapeLayer.fillColor = [UIColor greenColor].CGColor;
+    minuShapeLayer.lineWidth = 1;
+    minuShapeLayer.path = minuPath.CGPath;
+    [backgroundLayer addSublayer:minuShapeLayer];
+    
+    //秒针
+    UIBezierPath *secondPath = [[UIBezierPath alloc]init];
+    [secondPath moveToPoint:CGPointMake(2, 0)];
+    [secondPath addLineToPoint:CGPointMake(0, 75)];
+    [secondPath addLineToPoint:CGPointMake(4, 75)];
+    [secondPath addLineToPoint:CGPointMake(2, 0)];
+    secondShapeLayer = [CAShapeLayer layer];
+    secondShapeLayer.anchorPoint = CGPointMake(0.5f, 1.0f);
+    secondShapeLayer.frame = CGRectMake(98, 25, 4, 75);
+    secondShapeLayer.strokeColor = [UIColor blueColor].CGColor;
+    secondShapeLayer.fillColor = [UIColor blueColor].CGColor;
+    secondShapeLayer.lineWidth = 1;
+    secondShapeLayer.path = secondPath.CGPath;
+    NSLog(@"---%@",NSStringFromCGRect(secondShapeLayer.frame));
+    [backgroundLayer addSublayer:secondShapeLayer];
+    
+    //center point
+    UIBezierPath *pathCenterShaperLayer = [[UIBezierPath alloc]init];
+    [pathCenterShaperLayer addArcWithCenter:CGPointMake(100, 100) radius:10 startAngle:0 endAngle:2*M_PI clockwise:YES];
+    CAShapeLayer *centerShaperLayer = [CAShapeLayer layer];
+    centerShaperLayer.strokeColor = [UIColor redColor].CGColor;
+    centerShaperLayer.fillColor = [UIColor blackColor].CGColor;
+    centerShaperLayer.lineWidth = 5;
+    centerShaperLayer.path = pathCenterShaperLayer.CGPath;
+    [backgroundLayer addSublayer:centerShaperLayer];
     
     
-    self.timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(tick) userInfo:nil repeats:YES];
-    [self tick];
-    
-    [self setMask];
+    [self.view.layer addSublayer:backgroundLayer];
 }
--(void)tick{
+-(void)ticker{
     NSCalendar *calendar = [[NSCalendar alloc]initWithCalendarIdentifier:NSGregorianCalendar];
-    NSUInteger unites = NSHourCalendarUnit | NSMinuteCalendarUnit | NSSecondCalendarUnit;
-    NSDateComponents *components = [calendar components:unites fromDate:[NSDate date]];
-    CGFloat hoursAngle = (components.hour /12.0) * M_PI *2.0;
-    CGFloat minsAngle = (components.minute / 60.0) * M_PI *2.0;
-    CGFloat secsAngle = (components.second / 60.0) * M_PI *2.0;
+    NSUInteger units = NSHourCalendarUnit | NSMinuteCalendarUnit | NSSecondCalendarUnit;
+    NSDateComponents *components = [calendar components:units fromDate:[NSDate date]];
+    CGFloat hoursAngle = (components.hour/12.0) * M_PI * 2.0;
+    CGFloat minusAngle = (components.minute/60.0) * M_PI * 2.0;
+    CGFloat secondsAngle = (components.second/60.0) * M_PI * 2.0;
+    hourShapeLayer.transform = CATransform3DMakeRotation(hoursAngle, 0, 0, 1);
+    minuShapeLayer.transform = CATransform3DMakeRotation(minusAngle, 0, 0, 1);
+    secondShapeLayer.transform = CATransform3DMakeRotation(secondsAngle, 0, 0, 1);
     
-    self.hourHand.transform = CGAffineTransformMakeRotation(hoursAngle);
-    self.minuteHand.transform = CGAffineTransformMakeRotation(minsAngle);
-    self.secondHand.transform = CGAffineTransformMakeRotation(secsAngle);
 }
--(void)setMask{
-    UIView *view = [[UIView alloc]init];
-    view.layer.frame = CGRectMake((UISCREEN_WIDTH-100)/2, (UISCREEN_HEIGHT-100)/2, 100, 100);
-    view.backgroundColor = [UIColor yellowColor];
-    [UIView animateWithDuration:0.5 animations:^{
-        view.frame = CGRectMake(0, 0, UISCREEN_WIDTH, UISCREEN_HEIGHT);
-    }];
-    
-    [UIView animateWithDuration:0.5 delay:0.5 options:UIViewAnimationOptionCurveEaseInOut animations:^{
-        view.frame = CGRectMake(0, 0, 100, 100);
-    } completion:^(BOOL finished) {
-        
-    }];
-    [UIView animateWithDuration:0.5 delay:1 options:UIViewAnimationOptionCurveEaseInOut animations:^{
-        view.frame = CGRectMake(0, 0, UISCREEN_WIDTH, UISCREEN_HEIGHT);
-    } completion:^(BOOL finished) {
-        
-    }];
-    
-    
-    self.view.layer.mask = view.layer;
-}
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+/*
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 @end
